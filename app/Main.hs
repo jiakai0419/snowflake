a25a769a0c6d21d66add9621f2b21ee1b12eb7af
jiakai0419 <jiakai0419@gmail.com>
2016-09-04 14:22:21 +0800

    refacotring

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Data.Time.Exts.Unix
 worker1 = IdWorker { _sequence = 0
                    , _workerId = 7
                    , _datacenterId = 23
-                   , _lastTimestamp = 0
+                   , _timestamp = 0
                    , _conf = defaultConf
                    }
 
--- src/Snowflake.hs
+++ src/Snowflake.hs
@@ -7,61 +7,43 @@ module Snowflake
   , next
   ) where
 
+
 import Prelude hiding (sequence)
 import Data.Int (Int64)
 import Data.Maybe (Maybe)
 import Data.Bits ( (.|.)
                  , shiftL
                  )
-import Lens.Micro hiding (element)
-import Lens.Micro.TH
-import Lens.Micro.Extras
+
 
 type Timestamp = Int64
 
 data Conf = Conf { _sequenceBits :: Int
                  , _workerIdBits :: Int
                  , _datacenterIdBits :: Int
-                 , _timestampBase :: Timestamp
+                 , _twepoch :: Timestamp  -- timestamp base-line
                  } deriving (Show)
 
 data IdWorker = IdWorker { _sequence :: Int64
                          , _workerId :: Int64
                          , _datacenterId :: Int64
-                         , _lastTimestamp :: Timestamp
+                         , _timestamp :: Timestamp
                          , _conf :: Conf
                          } deriving (Show)
-
-makeLenses ''Conf
-makeLenses ''IdWorker
 
 defaultConf = Conf { _sequenceBits = 12
                    , _workerIdBits = 5
                    , _datacenterIdBits = 5
-                   , _timestampBase = 1472733628921
+                   , _twepoch = 1472733628921
                    }
 
 next :: IdWorker -> Timestamp -> (Maybe Int64, IdWorker)
-next worker timestamp =
-  if sequenceIsFull worker timestamp
-     then (Nothing, worker)
-     else (Just $ next' worker timestamp, worker {_sequence = newSequence worker timestamp, _lastTimestamp = timestamp})
-
-next' :: IdWorker -> Timestamp -> Int64
-next' worker timestamp = tBit .|. dBit .|. wBit .|. sBit
-  where sBit = newSequence worker timestamp
-        wBit = (worker ^. workerId) `shiftL` wShift
-        dBit = (worker ^. datacenterId) `shiftL` dShift
-        tBit = (timestamp - worker ^. conf . timestampBase) `shiftL` tShift
-        wShift = worker ^. conf . sequenceBits
-        dShift = wShift + (worker ^. conf . workerIdBits)
-        tShift = dShift + (worker ^. conf . datacenterIdBits)
-
-sequenceIsFull :: IdWorker -> Timestamp -> Bool
-sequenceIsFull worker timestamp = timestamp == (worker ^. lastTimestamp) && (worker ^. sequence) == 2^(worker ^. conf . sequenceBits) - 1
-
-newSequence :: IdWorker -> Timestamp -> Int64
-newSequence worker timestamp =
-  if timestamp == (worker ^. lastTimestamp)
-     then (worker ^. sequence) + 1
-     else 0
+next worker@(IdWorker lastSq wid did lastTs (Conf sBits wBits dBits twepoch)) ts
+  | lastTs == ts && lastSq == 2^sBits - 1 = (Nothing, worker)
+  | otherwise = (Just newId, worker {_sequence = newSq, _timestamp = ts})
+    where
+      newSq = if lastTs == ts then lastSq + 1 else 0
+      newId = tBit .|. dBit .|. wBit .|. newSq
+      wBit = wid `shiftL` sBits
+      dBit = did `shiftL` (sBits + wBits)
+      tBit = (ts - twepoch) `shiftL` (sBits + wBits + dBits)
